0f26cbb109ebbb17b15980206be73bf8e82d4a38
dilan <dilandeornelas@gmail.com>
2024-02-08 19:53:22 -0400

    Add new sprite assets and HUD script

---
+++ Assets/scrips/HUD.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+  
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpadateScore(int score)
+    {
+        Debug.Log("Score: " + score);
+    }
+
+    
+
+
+}
