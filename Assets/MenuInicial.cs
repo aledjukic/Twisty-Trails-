8f859bde1a5e9bd46a030909d9a22a49c89b1741
YGonza22 <y.gonzalez@correo.unimet.edu.ve>
2024-02-15 01:02:01 -0400

    Creacion del Menu

---
+++ Assets/MenuInicial.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuInicial : MonoBehaviour
+{
+    public void StartGame(){
+        //aqui va la escena del juego//
+        SceneManager.LoadScene("Yargen");
+    }
+
+    public void Quit(){
+        Debug.Log("Se ha salido del juego");
+        Application.Quit();
+    }
+}
